ccffb801394932af46df6081530ea312eba2489b
Marco Munizaga <git@marcopolo.io>
2021-03-16 20:58:27 -0700

    Add devshell

--- trivial/flake.nix
+++ trivial/flake.nix
@@ -1,11 +1,20 @@
 {
   description = "A very basic flake";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.hello = nixpkgs.legacyPackages.${system}.hello;
+        defaultPackage = self.packages.${system}.hello;
+        devShell =
+          let
+            pkgs = nixpkgs.legacyPackages.${system}; in
+          pkgs.mkShell {
+            buildInputs = [ pkgs.hello ];
+          };
+      });
 }
